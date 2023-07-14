2218e31709e2be93ebb718ebc6628f96cb3b4751
like a Boss <aghilas.smail@gmail.com>
2023-07-14 22:44:35 +0200

    add some comment to basic code

--- PYTHON TRAIN/cours 1/1.py
+++ PYTHON TRAIN/cours 1/1.py
@@ -2,12 +2,16 @@
 # test completed with succes let's gooooooooo
 def StringChallenge(sen):
 
+    # spité la phrase a des mots
     words = sen.split()
-    
+    # définir une variable au en va stocké le mote recent et celui ci en va l'afficher a la fin
     longest_word = ""
-
+    # Une boucle pour parcouir la liste des mots possible
     for word in words:
+        # Faire une compaaraison entre le mots actuel et l'anciane mote.
+        
         if len(word) > len(longest_word):
             longest_word = word
 
+    # Retourner le mots le plus grande.
     return longest_word
