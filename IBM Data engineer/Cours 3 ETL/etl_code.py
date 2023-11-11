df9efb368dc25d8375fb1a5ea18ba99d29ad23a1
like a Boss <aghilas.smail@gmail.com>
2023-11-11 09:10:26 +0100

    version 0.2

--- IBM Data engineer/Cours 3 ETL/etl_code.py
+++ IBM Data engineer/Cours 3 ETL/etl_code.py
@@ -35,4 +35,42 @@ def extract_from_xml(file_path):
 # A function how englobe all the files (CSV, JSON and XML)
 
 def extract():
+    extracted_data = pd.DataFrame(columns=['name','height','weight']) # create an empty data frame to hold extracted data 
+     
+    # process all csv files 
+    for csvfile in glob.glob("*.csv"): 
+        extracted_data = pd.concat([extracted_data, pd.DataFrame(extract_from_csv(csvfile))], ignore_index=True) 
+         
+    # process all json files 
+    for jsonfile in glob.glob("*.json"): 
+        extracted_data = pd.concat([extracted_data, pd.DataFrame(extract_from_json(jsonfile))], ignore_index=True) 
+     
+    # process all xml files 
+    for xmlfile in glob.glob("*.xml"): 
+        extracted_data = pd.concat([extracted_data, pd.DataFrame(extract_from_xml(xmlfile))], ignore_index=True) 
+         
+    return extracted_data 
+
+
+def transform(data): 
+    '''Convert inches to meters and round off to two decimals 
+    1 inch is 0.0254 meters '''
+    data['height'] = round(data.height * 0.0254,2) 
+ 
+    '''Convert pounds to kilograms and round off to two decimals 
+    1 pound is 0.45359237 kilograms '''
+    data['weight'] = round(data.weight * 0.45359237,2) 
     
+    return data 
+
+def load_data(target_file, transformed_data): 
+    transformed_data.to_csv(target_file) 
+    
+    
+def log_progress(message): 
+    timestamp_format = '%Y-%h-%d-%H:%M:%S' # Year-Monthname-Day-Hour-Minute-Second 
+    now = datetime.now() # get current timestamp 
+    timestamp = now.strftime(timestamp_format) 
+    with open(log_file,"a") as f: 
+        f.write(timestamp + ',' + message + '\n') 
+        
