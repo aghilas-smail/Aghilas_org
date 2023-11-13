56dcb5166bfb834a76baf0828e1c36f200415782
like a Boss <aghilas.smail@gmail.com>
2023-11-13 08:14:57 +0100

    ETL work

--- IBM Data engineer/Cours 3 ETL/etl_code.py
+++ IBM Data engineer/Cours 3 ETL/etl_code.py
@@ -1,55 +1,82 @@
-import glob 
-import pandas as pd 
-import xml.etree.ElementTree as ET 
-from datetime import datetime 
+import glob
+import pandas as pd
+import xml.etree.ElementTree as ET
+from datetime import datetime
 
+log_file = "log_file.txt"
+target_file = "transformed_data.csv"
 
-log_file = "log_file.txt" 
-target_file = "transformed_data.csv" 
-
-# A function for the csv file.
+# Function to extract data from CSV files
 def extract_from_csv(file_path):
-    dataframe = pd.read_csv(file_path)
-    return dataframe
+    try:
+        dataframe = pd.read_csv(file_path)
+        if not dataframe.empty:
+            return dataframe
+        else:
+            print(f"Empty CSV file: {file_path}")
+            return None
+    except pd.errors.EmptyDataError:
+        print(f"Empty CSV file: {file_path}")
+        return None
+    except Exception as e:
+        print(f"Error reading CSV file: {file_path}, Error: {str(e)}")
+        return None
 
-# A function for the json file.
+# Function to extract data from JSON files
 def extract_from_json(file_path):
-    dataframe = pd.read_json(file_path, line=True)
-    return dataframe
+    try:
+        dataframe = pd.read_json(file_path, lines=True)  # Use lines=True for JSON lines format
+        if not dataframe.empty:
+            return dataframe
+        else:
+            print(f"Empty JSON file: {file_path}")
+            return None
+    except Exception as e:
+        print(f"Error reading JSON file: {file_path}, Error: {str(e)}")
+        return None
 
-# A function for the json file.
-
+# Function to extract data from XML files
 def extract_from_xml(file_path):
-    dataframe = pd.DataFrame(columns=["name","height","weight"])
-    tree = ET.parse(file_path) 
-    root = tree.getroot() 
-    for person in root: 
-        name = person.find("name").text 
-        height = float(person.find("height").text) 
-        weight = float(person.find("weight").text) 
-        # if he find the 3 variables (name, height and weight he return the datafram)
-        dataframe = pd.concat([dataframe, pd.DataFrame([{"name":name, "height":height, "weight":weight}])], ignore_index=True) 
-    return dataframe
+    try:
+        dataframe = pd.DataFrame(columns=["name", "height", "weight"])
+        tree = ET.parse(file_path)
+        root = tree.getroot()
+        for person in root:
+            name = person.find("name").text
+            height = float(person.find("height").text)
+            weight = float(person.find("weight").text)
+            dataframe = pd.concat([dataframe, pd.DataFrame([{"name": name, "height": height, "weight": weight}])], ignore_index=True)
+        return dataframe
+    except Exception as e:
+        print(f"Error reading XML file: {file_path}, Error: {str(e)}")
+        return None
 
-
-# A function how englobe all the files (CSV, JSON and XML)
-
+# Function to extract data from all supported file types
 def extract():
-    extracted_data = pd.DataFrame(columns=['name','height','weight']) # create an empty data frame to hold extracted data 
-     
-    # process all csv files 
-    for csvfile in glob.glob("*.csv"): 
-        extracted_data = pd.concat([extracted_data, pd.DataFrame(extract_from_csv(csvfile))], ignore_index=True) 
-         
-    # process all json files 
-    for jsonfile in glob.glob("*.json"): 
-        extracted_data = pd.concat([extracted_data, pd.DataFrame(extract_from_json(jsonfile))], ignore_index=True) 
-     
-    # process all xml files 
-    for xmlfile in glob.glob("*.xml"): 
-        extracted_data = pd.concat([extracted_data, pd.DataFrame(extract_from_xml(xmlfile))], ignore_index=True) 
-         
-    return extracted_data 
+    extracted_data = []  # Use a list for more efficient concatenation
+
+    # Process all CSV files
+    for csvfile in glob.glob("*.csv"):
+        data = extract_from_csv(csvfile)
+        if data is not None:
+            extracted_data.append(data)
+
+    # Process all JSON files
+    for jsonfile in glob.glob("*.json"):
+        data = extract_from_json(jsonfile)
+        if data is not None:
+            extracted_data.append(data)
+
+    # Process all XML files
+    for xmlfile in glob.glob("*.xml"):
+        data = extract_from_xml(xmlfile)
+        if data is not None:
+            extracted_data.append(data)
+
+    if extracted_data:
+        return pd.concat(extracted_data, ignore_index=True)  # Concatenate the list of dataframes
+    else:
+        return pd.DataFrame(columns=["name", "height", "weight"])  # Return an empty DataFrame if no data
 
 
 def transform(data): 
@@ -74,3 +101,31 @@ def log_progress(message):
     with open(log_file,"a") as f: 
         f.write(timestamp + ',' + message + '\n') 
         
+# Log the initialization of the ETL process 
+log_progress("ETL Job Started") 
+ 
+# Log the beginning of the Extraction process 
+log_progress("Extract phase Started") 
+extracted_data = extract() 
+ 
+# Log the completion of the Extraction process 
+log_progress("Extract phase Ended") 
+ 
+# Log the beginning of the Transformation process 
+log_progress("Transform phase Started") 
+transformed_data = transform(extracted_data) 
+print("Transformed Data") 
+print(transformed_data) 
+ 
+# Log the completion of the Transformation process 
+log_progress("Transform phase Ended") 
+ 
+# Log the beginning of the Loading process 
+log_progress("Load phase Started") 
+load_data(target_file,transformed_data) 
+ 
+# Log the completion of the Loading process 
+log_progress("Load phase Ended") 
+ 
+# Log the completion of the ETL process 
+log_progress("ETL Job Ended") 
