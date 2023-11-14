dabd40e23b56764d70ae056f99f72f1854ea468d
aghilas1999 <aghilas.smail@gmail.com>
2023-11-14 22:39:28 +0100

    process of the ceation of the jupyter nootbook

--- 8. Segmentation IM/data.py
+++ 8. Segmentation IM/data.py
@@ -2,25 +2,3 @@
 Le lien pour télécharger le dataset(jeux de donnée).
 https://www.kaggle.com/datasets/kmader/finding-lungs-in-ct-data/
 '''
-import os
-import numpy as np
-import cv2
-import pandas as pd
-import sys
-import PIL.Image as Image
-import matplotlib.pyplot as plt
-import matplotlib as mpl
-
-
-image_path = ''
-image_save_path = ''
-
-image_ligne = 420
-image_col = 580
-
-# Crée une fonction de train pour la data.
-def create_train_data():
-    train_image_path = os.path.join(image_path,'train')
-    images = os.listdir(train_image_path)
-    total = len(images) / 2
-
