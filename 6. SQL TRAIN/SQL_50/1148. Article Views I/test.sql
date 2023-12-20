c7edf2170a84745b8b7280e788814e072a00871d
like a Boss <aghilas.smail@gmail.com>
2023-12-20 14:56:20 +0100

    invalid tweets

--- 6. SQL TRAIN/SQL_50/1148. Article Views I/test.sql
+++ 6. SQL TRAIN/SQL_50/1148. Article Views I/test.sql
@@ -1,5 +1,5 @@
-# Write your MySQL query statement below
-# https://leetcode.com/problems/article-views-i/?envType=study-plan-v2&envId=top-sql-50
+-- Write your MySQL query statement below
+-- https://leetcode.com/problems/article-views-i/?envType=study-plan-v2&envId=top-sql-50
 
 select DISTINCT  author_id as id 
 from Views
