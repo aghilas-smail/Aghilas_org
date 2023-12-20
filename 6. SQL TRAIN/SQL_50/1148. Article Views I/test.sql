6036803cce59e4f24f03a5cb00e5f62519febb87
like a Boss <aghilas.smail@gmail.com>
2023-12-20 14:49:08 +0100

    article views

---
+++ 6. SQL TRAIN/SQL_50/1148. Article Views I/test.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+# https://leetcode.com/problems/article-views-i/?envType=study-plan-v2&envId=top-sql-50
+
+select DISTINCT  author_id as id 
+from Views
+where author_id = viewer_id
+order by id asc
