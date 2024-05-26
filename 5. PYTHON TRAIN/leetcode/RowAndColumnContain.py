e55f67bd5076ed0a7ee5159c9cf3fc69ac7b38b5
Aghilas Smail <59511179+aghilas1999@users.noreply.github.com>
2024-05-26 14:13:36 +0200

    Row and Column

--- 5. PYTHON TRAIN/leetcode/RowAndColumnContain.py
+++ 5. PYTHON TRAIN/leetcode/RowAndColumnContain.py
@@ -12,3 +12,23 @@ Hence, we return true.
 """
 
 
+def Solution(matrix: list[list[int]]) -> bool:
+    
+    n = len(matrix)
+    n2 = set(range(1, n + 1))
+    
+    for i in range(n): 
+        
+        row = set(matrix[i])
+        col = set(matrix[i][j] for j in range(n))
+        
+        if set(row) != n2:
+            return False
+        if set(col) != n2:
+            return False
+        
+    return True
+
+
+matrix = [[1, 2, 3], [3, 1, 2], [2, 3, 1]]
+print(Solution(matrix))  
