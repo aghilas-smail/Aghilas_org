92fdf01929be1bffd2c1127c0edce933efcf3860
Aghilas Smail <59511179+aghilas1999@users.noreply.github.com>
2024-06-02 23:41:22 +0200

    Two_sum done and it's wirk

--- 5. PYTHON TRAIN/leetcode/Two_sum.py
+++ 5. PYTHON TRAIN/leetcode/Two_sum.py
@@ -5,8 +5,8 @@ class Solution(object):
         for i, num in enumerate(nums):
             nums_idice[num] = i
             
-        nums.sort()
-        
+        nums = sorted(nums)
+        # print(nums)
         # We start on the first element of the list
         left = 0
         # we start of the end of our list 
@@ -20,9 +20,30 @@ class Solution(object):
             if sum == target : 
                 index1 = nums_idice[nums[left]]
                 index2 = nums_idice[nums[right]]
+                # print(index1, index2)
+                if index1 == index2:
+                    index1 -=1
                 return [index1,index2]
+                
             
             elif sum < target:
                 left = left + 1
             else: 
                 right= right -1
+            
+      
+        
+
+solution = Solution()
+
+nums = [2,7,11,15]
+target = 9
+print(solution.twosum(nums,target))
+
+nums1 = [3,2,4]
+target = 6
+print(solution.twosum(nums1,target))
+
+nums2 = [3,3]
+target = 6
+print(solution.twosum(nums2,target))
