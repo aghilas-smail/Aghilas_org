d233c52d403a2102dd384713c44c861727f1cdc3
Aghilas Smail <59511179+aghilas-smail@users.noreply.github.com>
2024-08-22 14:56:09 +0200

    Create 1934._Confirmation_Rate.sql (problem solving in leetcode)

---
+++ SQL_TRAIN/SQL_50/1934._Confirmation_Rate.sql
@@ -0,0 +1,15 @@
+# Write your MySQL query statement bese
+select S.user_id,
+
+        round( -- Handles cases where the user has no confirmation messages by setting the rate to 0.
+            coalesce(count(case when C.action = 'confirmed' then 1 end) / nullif(count(C.user_id), 0), 0),
+            2 -- the round for getting value in decimal
+        ) as confirmation_rate
+from
+    Signups S
+left join 
+    Confirmations C
+on 
+    S.user_id = C.user_id
+Group BY
+    S.user_id;
