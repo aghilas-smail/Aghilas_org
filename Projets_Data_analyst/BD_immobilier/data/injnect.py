9407a8e3771a1bd27534b1246faf2e8f23029e2b
Smail <aghilas.smail@gmail.com>
2025-02-16 23:45:28 +0100

    injecter tous les données dans postgresql

--- Projets_Data_analyst/BD_immobilier/data/injnect.py
+++ Projets_Data_analyst/BD_immobilier/data/injnect.py
@@ -4,16 +4,16 @@ conn = psycopg2.connect("dbname=projectdb user=postgres password=Aghilas1999.")
 cur = conn.cursor()
 
 tables = {
-    "region": "region.csv",
-    "commune": "commune.csv",
-    "populationn": "populationn.csv",
-    "bien": "bien.csv",
-    "vente": "vente.csv"
+    # "region": "region.csv",
+    # "commune": "commune.csv",
+    # "populationn": "populationn.csv",
+    #"bien": "bien_corr.csv"
+    "vente": "vente_corr.csv"
 }
 
 for table, file in tables.items():
     with open(file, 'r') as f:
-        cur.copy_expert(f"COPY {table} FROM STDIN WITH (FORMAT csv, HEADER true, DELIMITER ';', ENCODING 'UTF8')", f)
+        cur.copy_expert(f"COPY {table} FROM STDIN WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8')", f)
     conn.commit()
 
 cur.close()
